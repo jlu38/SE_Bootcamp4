500b0806156477585808259f3c1cea7c2e85421d
jlu38 <joeylu38@gmail.com>
2019-10-29 11:42:15 -0400

    add viewbuilding

--- src/components/ViewBuilding.js
+++ src/components/ViewBuilding.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
 class ViewBuilding extends React.Component {
+
 	render() {
-		return (
+		const { data, selectedBuilding } = this.props;
+
+
+		const building = data
+			.filter(bData => {
+				return bData.id == selectedBuilding
+			})
+			.map(bData => {
+				return (
+					<ul>
+						<li>Code: {bData.code}</li>
+						<li>Name: {bData.name}</li>
+						<li>Address: {bData.address}</li>
+						<li>
+							Coordinates: {bData.coordinates && bData.coordinates.latitude + " "} 
+							{bData.coordinates && bData.coordinates.longitude}
+
+						</li>
+					</ul>
+				)
+			})
+
+		return(
 			<div>
 				<p>
-					{' '}
 					<i>Click on a name to view more information</i>
 				</p>
+				{building}
 			</div>
-		);
+		)
 	}
 }
 export default ViewBuilding;
